22227f72d6eb90865dd05246aa41288f743c5fca
Kyle Burt <arrant-pettier2u@icloud.com>
2025-12-17 11:53:39 -0800

    Add todo item logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./styles.css";
+import { Todo } from "./todo.js";
 
 
 console.log('hello world');
+
+const item = new Todo("Name", "Desc", "Prio", "Due Date");
+
+console.log(item.name);
+item.name = "test";
+console.log(item.name);
---
+++ src/todo.js
@@ -0,0 +1,36 @@
+class Todo {
+    constructor(name, description, priority, due) {
+        this._name = name;
+        this._description = description;
+        this._priority = priority;
+        this._due = due;
+    }
+    // Getters
+    get name() {
+        return this._name;
+    }
+    get description() {
+        return this._description;
+    }
+    get priority() {
+        return this._priority;
+    }
+    get due() {
+        return this._due;
+    }
+    //Setters
+    set name(name) {
+        this._name = name;
+    }
+    set description(description) {
+        this._description = description;
+    }
+    set priority(priority) {
+        this._priority = priority;
+    }
+    set due(due) {
+        this._due = due;
+    }
+}
+
+export { Todo };
